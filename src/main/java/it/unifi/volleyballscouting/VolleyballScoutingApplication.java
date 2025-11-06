572f4d1ad9914088997ce5b6d03008be8d0f5a77
Gabryoo3 <bilanciagabriele@ikmail.com>
2025-11-06 22:01:29 +0100

    first commit

---
+++ src/main/java/it/unifi/volleyballscouting/VolleyballScoutingApplication.java
@@ -0,0 +1,13 @@
+package it.unifi.volleyballscouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleyballScoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleyballScoutingApplication.class, args);
+	}
+
+}
