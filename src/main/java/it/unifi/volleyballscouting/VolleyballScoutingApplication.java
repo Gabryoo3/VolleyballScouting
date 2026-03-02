bb5ad14d1a985417e86877dffddc5691c0101e4d
Gabriele <bilanciagabriele03@gmail.com>
2026-03-02 16:08:18 +0100

    funzionamento interfaccia
    Fatto funzionare interfaccia

--- src/main/java/it/unifi/volleyballscouting/VolleyballScoutingApplication.java
+++ src/main/java/it/unifi/volleyballscouting/VolleyballScoutingApplication.java
@@ -3,21 +3,23 @@ package it.unifi.volleyballscouting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
-@RestController
+@Controller
+
 public class VolleyballScoutingApplication{
 
     @GetMapping("/")
     public String home() {
-        return "Hello, Volleyball Scouting!";
+        return "index.html"; //Uso del ResponseBody restituisce quello specificato nella stringa, senza riporta la pagina
+        //della stringa
     }
-
-
     public static void main(String[] args) {
         SpringApplication.run(VolleyballScoutingApplication.class, args);
     }
